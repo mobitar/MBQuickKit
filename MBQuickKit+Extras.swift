5f1c584f8904e2a4ed94460fa771855e6594388b
Mo Bitar <me@bitar.io>
2016-01-11 12:09:23 -0600

    updates

--- MBQuickKit+Extras.swift
+++ MBQuickKit+Extras.swift
@@ -13,3 +13,12 @@ func TICK(){ startTime =  NSDate() }
 func TOCK(function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__){
     print("\(function) Time: \(-startTime.timeIntervalSinceNow)\nLine:\(line) File: \(file)")
 }
+
+func delay(delay:Double, closure:()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
+}
